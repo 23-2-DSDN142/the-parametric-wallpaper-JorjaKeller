c818223542f8e89f625bff6f664c4dfee690c205
JorjaKeller <jorja.keller@gmail.com>
2023-08-07 08:53:18 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -6,7 +6,7 @@ let rect_height = 20;
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -15,126 +15,192 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(240, 255, 240); 
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  let cloudColor = color('#f5fcff');
+  let skyColor = color('#3b2633');
+  let stegoGreen = color(210, 232, 155)
+  let stegoOrange = color(247, 180, 92)
+  let stegoGreenShadow = color(129, 153, 67)
+  let stegoOrangeShadow = color(207, 135, 41)
+  
+  let stegoPurple = color(211, 134, 235)
+  let stegoPurpleShadow = color(177, 75, 209)
+  let stegoYellow = color(255, 237, 171)
+  let stegoYellowShadow = color(222, 187, 106)
+  
+  let stegoBlue = color(132, 147, 245)
+  let stegoBlueShadow = color(77, 95, 214)
+  let stegoRed = color(255, 196, 196)
+  let stegoRedShadow = color(217, 121, 121)
+ 
+  noStroke()
+  fill(skyColor)
+  rect(100,100,200,200)
+  
+  
+  
+  if(random(1) > 0.3){
+    
+  noStroke()
+  fill(stegoGreenShadow)
+  triangle(30,150,35,110,80,110)
+  triangle(29.7,150,42,153,80,110)
+  triangle(115,150,105,120,130,120)
+  triangle(114.7,150,140,120,130,146)
+  triangle(115,150,115,130,126,140)
+  ellipse(125,138,10,10)
+  
+  fill(stegoOrangeShadow)
+  triangle(90,88,75,30,65,63)//Middle plate
+  triangle(89.7,88,74.7,30,100,57)
+  triangle(120,88,140,46,115,63)//Back plate one
+  triangle(119.7,88,139.7,46,140,76)
+  triangle(147,100,160,73,145,83)//Back plate two
+  triangle(146.7,100,159.7,73,160,90)
+  triangle(55,95,40,55,55,65)//Front plate
+  triangle(55.3,95,40.3,55,35,80)
+  ellipse(150,110,12,15)
+  ellipse(130,115,10,7)
+  ellipse(137,105,7,7)
+  
+  
+  fill(stegoGreen)
+  
+  ellipse(100,100,95,80)//Stego body
+  ellipse(25,120,35,30)//Stego head
+  triangle(15,110,75,66,75,135)//Stego neck
+  triangle(25,135,100,140,45,100)
+  triangle(185,95,165,125,175,100)//Stego tail
+  triangle(180,100,100,140,120,65)
+  triangle(165,125,100,140,180,98)
+  triangle(110,139,165,125,130,130)
+  rect(55,130,20,25)//Front leg
+  triangle(75,155,65,115,90,115)
+  triangle(148,148,115,120,145,120)//Back leg
+  triangle(148,148,160,140,155,120)
+  triangle(148,148,135,120,160,120)
+  
+  fill(stegoOrange)
+  triangle(100,88,85,30,75,63)//Middle plate
+  triangle(99.7,88,84.7,30,110,57)
+  triangle(130,88,150,46,125,63)//Back plate one
+  triangle(129.7,88,149.7,46,150,76)
+  triangle(157,100,170,73,155,83)//Back plate two
+  triangle(156.7,100,169.7,73,170,90)
+  triangle(65,95,50,55,65,65)//Front plate
+  triangle(65.3,95,50.3,55,45,80)
+  ellipse(150,110,12,15)
+  ellipse(130,115,10,7)
+  ellipse(137,105,7,7)
 
-
-  noStroke()//sky five
-fill(196, 238, 255)
-ellipse(5,20,35,35)
-ellipse(25,30,25,25)
-ellipse(65,45,70,70)
-ellipse(105,25,30,30)
-ellipse(135,25,45,20)
-ellipse(165,30,55,55)
-ellipse(190,35,30,30)
-ellipse(205,20,35,35)
-ellipse(25,10,7,7)
-
-noStroke()//sky four
-fill(177, 231, 252)
-ellipse(80,75,50,50)
-ellipse(15,60,55,55)
-ellipse(50,80,30,30)
-ellipse(45,60,15,15)
-ellipse(190,60,40,40)
-ellipse(160,75,50,50)
-ellipse(125,55,65,65)
-ellipse(215,60,55,55)
-ellipse(158,35,9,9)
-
-  noStroke()//sky three
-  fill(157, 218, 242)
-  ellipse(20,100,65,70)
-  ellipse(65,110,45,45)
-  ellipse(90,95,20,20)
-  ellipse(175,95,65,65)//
-  ellipse(220,100,65,70)
-  //ellipse(-35,95,65,65)
-  ellipse(77,83,8,8)
-  ellipse(145,75,5,5)
-  ellipse(68,84,5,5)
-
-noStroke() //sky two
-fill(138, 203, 235)
-ellipse(30,135,70,70)
-ellipse(5,110,20,20)
-ellipse(70,140,35,35)
-ellipse(105,130,50,45)
-ellipse(170,145,110,90)
-ellipse(205,110,20,20)
-ellipse(195,100,7,7)
-
-  noStroke()//sky one
-  fill(104, 176, 212)
-ellipse(7,175,80,90)
-ellipse(80,185,90,60)
-ellipse(140,170,85,70)
-ellipse(180,185,65,50)
-ellipse(100,150,30,30)
-ellipse(180,140,15,15)
-ellipse(187,130,5,5)
-ellipse(43,137,10,10)
-ellipse(207,175,80,90)
-
-noStroke()//sky six repeat
-fill(217, 244, 255)
-//ellipse(20,200,60,60)
-ellipse(220,200,60,60)
-ellipse(50,210,45,45)
-ellipse(90,210,50,50)
-ellipse(135,215,70,70)
-ellipse(175,190,45,45)
-ellipse(150,185,20,20)
-ellipse(67,197,10,10)
-ellipse(110,190,30,35)
-ellipse(125,173,7,7)
-ellipse(132,176,4,4)
-ellipse(50,183,5,5)
-
-
-fill(cloudColor)
-noStroke()
-  ellipse(100,110,30,20)//cloud one
-ellipse(120,97,40,50)
-ellipse(155,106,60,35)
-ellipse(184,113,17,12)
-ellipse(135,117,15,10)
-ellipse(142,87,10,10)
-
-ellipse(40,165,40,20)//cloudtwo
-ellipse(60,155,35,40)
-ellipse(40,153,12,12)
-ellipse(80,160,55,30)
-
-ellipse(60,35,30,25)//cloudthree
-ellipse(45,40,15,13)
-ellipse(75,40,27,13)
-ellipse(77,33,7,7)
-
-ellipse(180,180,20,15)//cloudfour
-ellipse(190,170,25,30)
-ellipse(200,175,30,20)
-
-ellipse(0,175,30,20)//cloudfour repeat
-ellipse(-10,170,25,30)
-
-ellipse(160,52,15,20)//cloudfive
-ellipse(167,54,12,12)
-ellipse(157,56,25,10)
-ellipse(155,52,10,10)
-
-ellipse(30,72,15,20)//cloudfive copy one
-ellipse(37,74,12,12)
-ellipse(27,76,25,10)
-ellipse(25,72,10,10)
-
-ellipse(140,152,15,20)//cloud five copy two
-ellipse(147,154,12,12)
-ellipse(137,156,25,10)
-ellipse(135,152,10,10)
-
-}
+  if(random(1) > 0.6){
+    noStroke()
+  fill(stegoBlueShadow)
+  triangle(30,150,35,110,80,110)
+  triangle(29.7,150,42,153,80,110)
+  triangle(115,150,105,120,130,120)
+  triangle(114.7,150,140,120,130,146)
+  triangle(115,150,115,130,126,140)
+  ellipse(125,138,10,10)
+  
+  fill(stegoRedShadow)
+  triangle(90,88,75,30,65,63)//Middle plate
+  triangle(89.7,88,74.7,30,100,57)
+  triangle(120,88,140,46,115,63)//Back plate one
+  triangle(119.7,88,139.7,46,140,76)
+  triangle(147,100,160,73,145,83)//Back plate two
+  triangle(146.7,100,159.7,73,160,90)
+  triangle(55,95,40,55,55,65)//Front plate
+  triangle(55.3,95,40.3,55,35,80)
+  ellipse(150,110,12,15)
+  ellipse(130,115,10,7)
+  ellipse(137,105,7,7)
+  
+  
+  fill(stegoBlue)
+  
+  ellipse(100,100,95,80)//Stego body
+  ellipse(25,120,35,30)//Stego head
+  triangle(15,110,75,66,75,135)//Stego neck
+  triangle(25,135,100,140,45,100)
+  triangle(185,95,165,125,175,100)//Stego tail
+  triangle(180,100,100,140,120,65)
+  triangle(165,125,100,140,180,98)
+  triangle(110,139,165,125,130,130)
+  rect(55,130,20,25)//Front leg
+  triangle(75,155,65,115,90,115)
+  triangle(148,148,115,120,145,120)//Back leg
+  triangle(148,148,160,140,155,120)
+  triangle(148,148,135,120,160,120)
+  
+  fill(stegoRed)
+  triangle(100,88,85,30,75,63)//Middle plate
+  triangle(99.7,88,84.7,30,110,57)
+  triangle(130,88,150,46,125,63)//Back plate one
+  triangle(129.7,88,149.7,46,150,76)
+  triangle(157,100,170,73,155,83)//Back plate two
+  triangle(156.7,100,169.7,73,170,90)
+  triangle(65,95,50,55,65,65)//Front plate
+  triangle(65.3,95,50.3,55,45,80)
+  ellipse(150,110,12,15)
+  ellipse(130,115,10,7)
+  ellipse(137,105,7,7)}}
+  
+  else{
+    
+    noStroke()
+    fill(stegoPurpleShadow)
+    triangle(30,150,35,110,80,110)
+    triangle(29.7,150,42,153,80,110)
+    triangle(115,150,105,120,130,120)
+    triangle(114.7,150,140,120,130,146)
+    triangle(115,150,115,130,126,140)
+    ellipse(125,138,10,10)
+    
+    fill(stegoYellowShadow)
+    triangle(90,88,75,30,65,63)//Middle plate
+    triangle(89.7,88,74.7,30,100,57)
+    triangle(120,88,140,46,115,63)//Back plate one
+    triangle(119.7,88,139.7,46,140,76)
+    triangle(147,100,160,73,145,83)//Back plate two
+    triangle(146.7,100,159.7,73,160,90)
+    triangle(55,95,40,55,55,65)//Front plate
+    triangle(55.3,95,40.3,55,35,80)
+    ellipse(150,110,12,15)
+    ellipse(130,115,10,7)
+    ellipse(137,105,7,7)
+    
+    
+    fill(stegoPurple)
+    
+    ellipse(100,100,95,80)//Stego body
+    ellipse(25,120,35,30)//Stego head
+    triangle(15,110,75,66,75,135)//Stego neck
+    triangle(25,135,100,140,45,100)
+    triangle(185,95,165,125,175,100)//Stego tail
+    triangle(180,100,100,140,120,65)
+    triangle(165,125,100,140,180,98)
+    triangle(110,139,165,125,130,130)
+    rect(55,130,20,25)//Front leg
+    triangle(75,155,65,115,90,115)
+    triangle(148,148,115,120,145,120)//Back leg
+    triangle(148,148,160,140,155,120)
+    triangle(148,148,135,120,160,120)
+    
+    fill(stegoYellow)
+    triangle(100,88,85,30,75,63)//Middle plate
+    triangle(99.7,88,84.7,30,110,57)
+    triangle(130,88,150,46,125,63)//Back plate one
+    triangle(129.7,88,149.7,46,150,76)
+    triangle(157,100,170,73,155,83)//Back plate two
+    triangle(156.7,100,169.7,73,170,90)
+    triangle(65,95,50,55,65,65)//Front plate
+    triangle(65.3,95,50.3,55,45,80)
+    ellipse(150,110,12,15)
+    ellipse(130,115,10,7)
+    ellipse(137,105,7,7)}
+  
+  fill(0)
+  ellipse(22,125,5,5)}
